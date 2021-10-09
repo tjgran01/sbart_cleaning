8ed152200af513848aff63845a51d2a51b1c31e8
Trevor Grant <30323894+tjgran01@users.noreply.github.com>
2021-10-09 17:30:18 -0400

    began lite analysis folder

--- python/lite_analysis/main.py
+++ python/lite_analysis/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 import seaborn as sns
 
 def main():
@@ -7,9 +8,16 @@ def main():
     FPATH = "../../test/sbart_balloon_features.csv"
 
     df = pd.read_csv(FPATH)
-    df = df[df["total_balloon_time (s)"] < 100]
-    ax = sns.histplot(df["total_balloon_time (s)"])
-    ax = sns.displot(df["total"])
+    df = df[df["total_balloon_time"] < 50]
+
+    model = sm.OLS(df["total_balloon_time"], df["number_of_tokens"]).fit()
+    predictions = model.predict(df["total_balloon_time"])
+
+    sns.histplot(predictions)
+    print(model.summary())
+
+
+    # ax = sns.histplot(df["hold_ratio"])
     plt.show()
 
 
